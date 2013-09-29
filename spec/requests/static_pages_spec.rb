3087650db9c90d3eb5afd45c27be4f4bad79be42
Roberto Pezzali <scrivoaroby@gmail.com>
2013-09-29 21:31:24 +0200

    Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Homepage" do
+
+	it "should have the content Sample App" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the content" do
+	    visit '/static_pages/home'
+	    expect(page).to have_title('Home')
+    end
+  end
+
+   describe "Help page" do
+    it "should have the content" do
+	    visit '/static_pages/help'
+	    expect(page).to have_title('Help')
+    end
+
+    it "should have the content Help" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+  end
+
+  describe "About page" do
+
+  	it "should have the content About" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the content" do
+	    visit '/static_pages/about'
+	    expect(page).to have_title('About')
+    end
+  end
+
+  describe "Contact page" do
+
+	it "should have the content Contact" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the content" do
+	    visit '/static_pages/contact'
+	    expect(page).to have_title('Contact')
+    end
+  end
+
+end
